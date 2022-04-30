e170b4f2415d7e2f2489bf68e4d2d8e6775346f7
Alvin Cheng <chengalvin333@gmail.com>
2022-04-30 11:37:09 +1000

    Add port drivers

---
+++ src/kernel/drivers/port.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define LOW_BYTE 15
+#define HIGH_BYTE 14
+
+#define RESULT_PORT 0x3d5
+#define COMMAND_PORT 0x3d4
+
+unsigned char port_byte_in(unsigned short port);
+void port_byte_out(unsigned short port, unsigned char data);
+unsigned short port_word_in(unsigned short port);
+void port_word_out(unsigned short port, unsigned short data);   
+
