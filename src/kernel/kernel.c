5da7b9d13ee3964a40c16547c37f4d9291a29373
Alvin Cheng <chengalvin333@gmail.com>
2022-06-04 11:28:17 +1000

    Update kernel.c

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,8 +1,10 @@
 #include "./drivers/screen.h"
 #include "./lib/ctypes.h"
+#include "./drivers/sound.h"
 
 void _start()
 {
     screenClear(0x00);
-    screenPrintString("Welcome to Roo-Os!", 0x0f, 1, 2);
+    printString("Welcome!", 0x0f, 9, 8);
+    printString("Welcome!", 0x0f, 1, 2);
 }
