30e69f18fcf0e5afa8a025c88addd2ce84ec39d3
Alvin Cheng <chengalvin333@gmail.com>
2022-06-05 17:12:11 +1000

    Update kernel.c

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,12 +1,12 @@
 #include "./drivers/screen.h"
 #include "./lib/ctypes.h"
 #include "./drivers/sound.h"
+#include "./interrupt/idt.h"
 
 void _start()
 {
     screenClear(0x00);
-    printString("Hello", 0x0f, 9, 8);
-    printString("pp", 0x0f, 9, 10);
-    printString("CC", 0x0f, 9, 12);
-    printString("Cpp", 0x0f, 9, 14);
+    prepareIDT();
+    printString("hello", 0x0f, 1, 0);
+    asm volatile("hlt");
 }
