5ea56f1ae16a8df1eeed7af64a77499faad326ea
Goomba <84668888+GoombaProgrammer@users.noreply.github.com>
2022-06-06 11:35:27 +0200

    Add comments

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,12 +1,18 @@
-#include "./drivers/screen.h"
+/*
+   This is the main file of the Roo-OS kernel.
+   This file includes the drivers, libraries, and then it
+   does all the kernel stuff.
+*/
+
+#include "./drivers/screen.h" // Screen Driver
 #include "./lib/ctypes.h"
 #include "./drivers/sound.h"
 #include "./interrupt/idt.h"
 
 void _start()
 {
-    screenClear(0x00);
+    screenClear(0x00);          // Clear the screen to hide BIOS messages.
     prepareIDT();
-    printString("hello", 0x0f, 1, 0);
+    printString("hello", 0x0f, 1, 0);       // Print 'Hello'.
     asm volatile("hlt");
 }
